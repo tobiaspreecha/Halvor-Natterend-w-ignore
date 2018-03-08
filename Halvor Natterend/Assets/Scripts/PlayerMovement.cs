16d3cc24ba407dd28d611a2d6e706295ea21e671
tobiaspreecha <craftyhorizonstudios@gmail.com>
2018-03-08 10:07:55 +0100

    1
    1

---
+++ Halvor Natterend/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float PlayerSpeed = 10.0f;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            
+        //Controls
+        var horizontal = Input.GetAxis("Horizontal");
+
+        gameObject.GetComponent<Animator>().SetBool("isWalking", Input.GetButtonDown("Horizontal"));
+        
+        //Physics
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(horizontal * PlayerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+}
