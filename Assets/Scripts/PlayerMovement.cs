e311b97e3d5a33fef81dbf87965781a859501b02
DankedMemeOfDoom <33284755+DankedMemeOfDoom@users.noreply.github.com>
2018-03-14 09:20:05 +0100

    Fixed Walking
    I did it

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMovement : MonoBehaviour
 {
 
@@ -23,29 +24,10 @@ public class PlayerMovement : MonoBehaviour
         gameObject.GetComponent<Animator>().SetBool("isWalking", Input.GetButtonDown("Horizontal"));
         gameObject.GetComponent<Animator>().SetBool("isWalking", Input.GetButtonDown("Vertical"));
         #endregion
-        #region Flip
-        //Animations
-        if (horizontal < -0.1f)
-        {
-            gameObject.GetComponent<SpriteRenderer>().flipX = false;
-        }
-        else if (horizontal > 0.1f)
-        {
-            gameObject.GetComponent<SpriteRenderer>().flipX = true;
-        }
-
-        if (vertical < -0.1f)
-        {
-            gameObject.GetComponent<SpriteRenderer>().flipY = true;
-        }
-        else if (vertical > 0.1f)
-        {
-            gameObject.GetComponent<SpriteRenderer>().flipY = false;
-        }
-        #endregion
+       
 
         //Physics
-        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(horizontal * PlayerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
-        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(vertical * PlayerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.x);
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(vertical * PlayerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(horizontal * PlayerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.x);
     }
 }
